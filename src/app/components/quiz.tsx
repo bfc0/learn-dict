86d513c7d4b769faa5415868d53b1b6f9526a974
bfc0 <bfcheg@gmail.com>
2023-11-11 23:55:19 +0300

    refactor

--- src/app/components/quiz.tsx
+++ src/app/components/quiz.tsx
@@ -20,6 +20,8 @@ export default function Quiz(props: { words: Word[] }) {
     const words = props.words
     const [quizItem, setQuizItem] = useState<QuizItem | null>(null)
     const [clickedItems, setClickedItems] = useState<Array<string>>([])
+    const [correctAnswers, setCorrectAnswers] = useState(0)
+    const [totalAnswers, setTotalAnswers] = useState(0)
 
     function getRandomIdx(list: Array<any>): number {
         return Math.floor(Math.random() * list.length)
@@ -46,7 +48,9 @@ export default function Quiz(props: { words: Word[] }) {
 
     function clickAnswer(answer: string) {
         setClickedItems(prevClickedItems => [...prevClickedItems, answer])
+        setTotalAnswers(prev => prev + 1)
         if (answer == quizItem?.correct) {
+            setCorrectAnswers(prev => prev + 1)
             setTimeout(() => {
                 setQuizItem(generateItemFrom(words))
                 setClickedItems([])
@@ -68,24 +72,32 @@ export default function Quiz(props: { words: Word[] }) {
         return () => window.removeEventListener("keydown", keyPress)
     }, [quizItem])
 
-    const buttonUnknown = "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-    const buttonWrong = "text-white border border-red-700 bg-red-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-    const buttonCorrect = "text-white border border-green-700 bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+    const buttonUnknown = "text-blue-700 hover:text-white  hover:bg-blue-800   font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+    const buttonWrong = "text-white   bg-red-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+    const buttonCorrect = "text-white  bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
 
     return (
         <div>
             {
                 quizItem && <>
-                    <h1 className="text-xl py-4 font-bold"> {quizItem.word}</h1>
+                    <h2 className="inline pl-5">Correct:</h2>
+                    <h1 className="inline pl-3 font-bold">{correctAnswers}</h1>
+                    <h2 className="inline pl-10">Total:</h2>
+                    <h1 className="inline pl-3 font-bold">{totalAnswers}</h1>
+
+
+                    <h1 className="text-xl py-16 px-5 font-bold"> {quizItem.word}</h1>
                     {
                         quizItem.answers.map(item =>
-                            <button
-                                className={
-                                    clickedItems.includes(item) ?
-                                        quizItem.correct === item ? buttonCorrect : buttonWrong
-                                        : buttonUnknown
-                                }
-                                onClick={() => clickAnswer(item)}>{item}</button>
+                            <div>
+                                <button
+                                    className={
+                                        clickedItems.includes(item) ?
+                                            quizItem.correct === item ? buttonCorrect : buttonWrong
+                                            : buttonUnknown
+                                    }
+                                    onClick={() => clickAnswer(item)}>{item}</button>
+                            </div>
                         )
                     }
                 </>
